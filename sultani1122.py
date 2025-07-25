ca4ea425e56702331b344a526c32806d81a5aacd
shahozangana5 <shahokaya777@gmail.com>
2025-07-25 13:40:22 +0300

    Add files via upload

---
+++ sultani1122.py
@@ -0,0 +1,35 @@
+دستورات استفاده  شده در آموزش بالا
+
+
+تهیه شده توسط محمد سلطانی
+
+
+1 termux-setup-storage
+
+2 cd $HOME
+rm -rf FILE
+git clone --depth=1 https://github.com/SHAJON-404/FILE.git
+cd FILE
+chmod 777 FILE
+./FILE
+
+3pkg install git
+
+4 pkg install python2
+
+5 git clone https://github.com/binyamin-binni/b-all
+
+6 cd b-all
+
+7 python2 b-all.py
+
+
+Username : binyamin
+
+Pass : b-all
+
+
+
+@sultani1122
+
+کانال تلگرام
